b2a96a41eff334fa06435574841a9e7671dd99ad
mehedi-hridoy <mehedi.hridoy101@gmail.com>
2025-11-20 00:59:04 +0600

    Social Linkes update

--- app/components/LeftCard.tsx
+++ app/components/LeftCard.tsx
@@ -23,37 +23,43 @@ export default function LeftCard() {
       {/* Profile Image */}
       <div className="w-[240px] h-[284px] rounded-2xl overflow-hidden mx-auto mb-8">
         <Image
-          src="/images/Mehedi_hridoy.png"   
+          src="/images/Mehedi_hridoy.png"
           alt="Mehedi Hasan Hridoy"
           width={240}
           height={284}
-          quality={100}
+          unoptimized={true}       // ⬅ load EXACT file, no optimization
           priority={true}
-          placeholder="empty"
           className="object-cover"
           style={{
-            imageRendering: "crisp-edges", 
+            imageRendering: "crisp-edges",    // force sharper rendering
           }}
         />
       </div>
 
+      {/* Name */}
       <h2 className="text-2xl font-semibold text-white mb-2">
         Mehedi Hasan Hridoy
       </h2>
 
+      {/* Title */}
       <p className="text-gray-300 text-sm">Full Stack Engineer</p>
 
+      {/* Location */}
       <p className="text-gray-500 text-xs mt-1">
         Dhaka, Bangladesh
       </p>
 
+      {/* Social Icons */}
       <div className="flex items-center justify-center gap-6 mt-6 text-white/80 text-[20px]">
-        <a href="#" className="hover:text-white transition"><FaGithub /></a>
-        <a href="#" className="hover:text-white transition"><FaXTwitter /></a>
-        <a href="#" className="hover:text-white transition"><FaInstagram /></a>
-        <a href="#" className="hover:text-white transition"><HiOutlineEnvelope /></a>
+        <a href="https://github.com/mehedi-hridoy" target="_blank" className="hover:text-white transition"><FaGithub /></a>
+        <a href="https://x.com/_Mehedi_Hridoy" target="_blank" rel="noopener noreferrer" className="hover:text-white transition">
+          <FaXTwitter />
+        </a>
+        <a href="https://www.instagram.com/mehedi_hasan_.hridoy/" target="_blank" className="hover:text-white transition"><FaInstagram /></a>
+        <a href="mailto:mehedi.hridoy101@gmail.com" className="hover:text-white transition"><HiOutlineEnvelope /></a>
       </div>
 
+      {/* Button */}
       <button
         className="
           mt-10
